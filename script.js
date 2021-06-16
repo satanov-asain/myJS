0c66ccd2b6c58c120bb5080fa93cdfb1bf9b7b13
Asain Satanov <satanov.asain@mail.ru>
2021-06-16 11:47:59 +0600

    Первое приложение

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-console.log(1);
-let a=10;
+const numberOfFilms = prompt();
+const personalMovieDB = {
+count: numberOfFilms,
+movies:{
+
+},
+actors:{},
+genres:[],
+privat:true
+};
+const lastFilm1=prompt("Последний фильм"),
+    bandFilm1=prompt("Оцените"),
+    lastFilm2=prompt("Последний фильм"),
+    bandFilm2=prompt("Оцените");
+personalMovieDB.movies[lastFilm1]=bandFilm1;
+personalMovieDB.movies[lastFilm2]=bandFilm2;
+console.log(personalMovieDB);
